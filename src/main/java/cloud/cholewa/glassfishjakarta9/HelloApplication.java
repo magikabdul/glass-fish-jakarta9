0e0511ab229910a867d2697c750bad55a04438a8
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-04-15 16:16:08 +0200

    [JAKARTA-9] starting configuration with simple HelloWorld

---
+++ src/main/java/cloud/cholewa/glassfishjakarta9/HelloApplication.java
@@ -0,0 +1,9 @@
+package cloud.cholewa.glassfishjakarta9;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
